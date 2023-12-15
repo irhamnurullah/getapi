f8a0128381af7294b7032ce44286ebbca1e7138f
irham nurullah <nurullahirham@gmail.com>
2023-12-15 14:22:27 +0700

    add cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  res.status(200).json({ name: 'John Doe' });
 }
