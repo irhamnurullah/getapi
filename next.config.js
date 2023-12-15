0f5b45c5148ed07b44f8b4d91b8edd1af3052fba
irham nurullah <nurullahirham@gmail.com>
2023-12-15 14:11:45 +0700

    init api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+const remotes = (isServer) => {
+  const location = isServer ? 'ssr' : 'chunks';
+  return {
+    remote: `remote@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  // standalone
+  output: 'standalone',
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'host',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: remotes(isServer),
+        exposes: {
+          // './components/Nav': './components/Nav',
+          // './components/Logo': './components/Logo',
+        },
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
