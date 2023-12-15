260640f7cdd0616b3d4718ebbff3ad5df11dead6
irham nurullah <nurullahirham@gmail.com>
2023-12-15 14:34:01 +0700

    remove cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import Cors from 'cors';
-
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-});
-
 export default async function handler(req, res) {
-  await cors(req, res);
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
